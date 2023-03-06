f48acb062d72dd967bf71b57da81f68a0bd0b46b
Marcus Faria <mfaria150.github@gmail.com>
2023-03-05 23:25:31 -0300

    feat(POST_INSTALLATION.sh): Set instruction to install packages
    Set instruction to install packages base.
    Set instruction to install packages dev.
    Set instruction to install lunar vim.
    Set instruction to install yay.
    Set instruction to call configure_lvim.sh.

--- POST_INSTALLATION.sh
+++ POST_INSTALLATION.sh
@@ -3,9 +3,39 @@
 # Set global variables
 COLOR_BLUE="\e[0;34m"
 COLOR_GRAY="\e[1;30m"
+COLOR_GREEN="\e[0;32m"
+COLOR_RED="\e[0;31m"
 STYLE_CLEAR="\e[0m"
 
 CURUSER=$(whoami)
+
+PKGS_BASE_DEVEL=(
+    base-devel
+    git
+    clang
+    curl
+    wget
+    zsh
+    openssh
+)
+
+PKGS_DEV=(
+    rust
+    yarn
+    npm
+    python
+    lua
+    neovim
+)
+
+PKG_POWERLEVEL="zsh-theme-powerlevel10k-git"
+
+FONTS=(
+    ttf-meslo-nerd-font-powerlevel10k
+    powerline-fonts
+    awesome-terminal-fonts
+    ttf-fira-code
+)
 
 # Main instructions
 if [[ $CURUSER == "root" ]]; then
@@ -42,4 +72,49 @@ if [[ $CURUSER == "root" ]]; then
         echo -e "\n$COLOR_GRAY--> $file_key_sign, file not found$STYLE_CLEAR"
         exit 1
     fi
+else
+    sudo pacman -Syyu
+
+    # --> Install base devel, and other base packages
+    echo -e "$COLOR_GREEN--> Install base packages$STYLE_CLEAR"
+    for curpkg in ${PKGS_BASE_DEVEL[@]}; do
+        sudo pacman -S --noconfirm --needed "$curpkg"
+    done
+
+    # --> Install programing language packages
+    echo -e "$COLOR_GREEN--> Install dev packages"
+    for curpkg in ${PKGS_DEV[@]}; do
+        sudo pacman -S --noconfirm --needed "$curpkg"
+    done
+
+    # --> Install Lunar vim
+    LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+
+    # --> Update file, configure Lunar vim
+    file_configure_lvim="configure_lvim.sh"
+    if [[ -f "$file_configure_lvim" ]]; then
+        echo -e "$COLOR_GRAY--> Configure lunar vim$STYLE_CLEAR"
+        "./$file_configure_lvim"
+    else
+        echo -e "$COLOR_GRAY--> $file_configure_lvim, file not found$STYLE_CLEAR"
+        exit 1
+    fi
+
+    # --> Install YAY, AUR helper
+    echo -e "$COLOR_GREEN--> Install YAY$STYLE_CLEAR"
+    cd /tmp/
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+
+    # --> Install theme to zsh shell
+    # --> script post-finalization, configure Theme: Use p10k configure
+    echo -e "$COLOR_GREEN--> Install Powerlevel10k theme"
+    yay -S --noconfirm "$PKG_POWERLEVEL"
+    for font in ${FONTS[@]}; do
+        yay -S --noconfirm "$font"
+    done
+
+    # --> Set zsh how default shell
+    chsh -s /usr/bin/zsh
 fi
